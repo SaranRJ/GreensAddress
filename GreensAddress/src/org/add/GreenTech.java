94d5476f0ab75d8b6305740368f12fbb0500565f
SaranRJ <90471894+SaranRJ@users.noreply.github.com>
2022-09-06 13:05:25 +0530

    Add files via upload

---
+++ GreensAddress/src/org/add/GreenTech.java
@@ -0,0 +1,37 @@
+package org.add;
+
+public class GreenTech {
+
+	public void greensOmr() {
+		System.out.println("GreenTech at Omr");
+	}
+	public void greensAdyar() {
+		System.out.println("GreenTech at Adyar");
+
+	}
+	public void greensTambaram() {
+	
+		System.out.println("GreenTech at Tambaram");
+	}
+	public void greensVelacherry() {
+	
+		System.out.println("GreenTech at Velacherry");
+	}
+	public void greensAnnaNagar() {
+		System.out.println("GreenTech at AnnaNagar");
+
+	}
+	
+	
+	public static void main(String[] args) {
+	
+
+		GreenTech GT=new GreenTech();
+		GT.greensOmr();
+		GT.greensTambaram();
+		GT.greensAdyar();
+		GT.greensVelacherry();
+		GT.greensAnnaNagar();
+	}
+
+}
